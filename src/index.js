93930c04e824b33f086203051f219a24f4a2548d
AnnaPlis1 <112053615+AnnaPlis1@users.noreply.github.com>
2023-01-12 09:40:11 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = n + "";
+    if (n>0){return Number(n.split("").reverse().join(""));}
+   else if (n<0){n = -n + "";
+    return Number(n.split("").reverse().join(""));
+}
 }
